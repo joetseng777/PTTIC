d05b960211aae67d08f9b285fe57da690ab0cd98
joetseng777 <badc01dww@gmail.com>
2024-08-28 16:26:33 +0800

    INIT0828

---
+++ Readme.md
@@ -0,0 +1,3 @@
+* pip list 調出版本
+* pip install -r requirements.txt
+* .env 做隱藏加密解密,可以做隱私跟資安防護,但效能會犧牲一點但防護會安全一點,=之間不要空格空白
--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import requests
-
-
+from bs4 import BeautifulSoup
+import time
+import pandas
+import datetime
+import json
+from dotenv import load_dotenv
+import os
+load_dotenv()
 """
 常數區塊
 """
@@ -29,13 +35,111 @@ page_num = ''
 # 合併完整路徑
 target = BASE_URL + target_board + TARGET_PAGE + page_num + HTML_EXT
 
+#下載目標程式檔
 def download_html(target, headers=HEADERS):
-    # data = requests.get(target, headers=headers)
-    # return data
     return requests.get(target, headers=headers)
 
-"""
-test
-"""
-x = download_html(target)
-print(x.content)
+
+# 分析 HTML Code 取得文章連結
+def get_article_url(html_code):
+    url_list = []
+    html_parser = BeautifulSoup(html_code.content, features="html.parser")
+    div_list = html_parser.find_all('div', class_="title")
+    for div_ in div_list:
+        try:
+            a_tag = div_.find('a').attrs['href']
+            url_list.append(a_tag)
+        except:
+            print('該目標看板有無法讀取的文章連結!')
+    return url_list
+
+# 讀取各文章資料
+def parser_article_content(url_list):
+    article_base_url = BASE_URL.replace('/bbs/', '')
+    ptt_data = []
+
+
+    for url_ in url_list:
+        if(url_) != None:
+            article_url = article_base_url + url_
+            page_data = download_html(article_url)
+            page_html_code = BeautifulSoup(page_data.content)
+            meta_info = page_html_code.find_all('span', class_='article-meta-value')
+            author = meta_info[0].contents[0]
+            title = meta_info[2].contents[0]
+            date = meta_info[3].contents[0]
+            article = page_html_code.find('div', id = 'main-content').contents[4]
+
+
+            # 建立字典型態
+            article_row = {
+                'url': article_url,
+                'title': title,
+                'author': author,
+                'date': date,
+                'content': article
+            }
+
+
+            # 加入 list 內
+            ptt_data.append(article_row)
+            print(author, title, date)
+            # 暫停
+            time.sleep(0.5)
+    return ptt_data
+
+# 儲存為 json 或 csv
+def save_result(data, format = 'json'):
+    data_df = pandas.DataFrame(data)
+    if format == 'json':
+        data_df.to_json('data-{}.json'.format(get_datetime_str()))
+    elif format == 'csv':
+        data_df.to_csv('data-{}.csv'.format(get_datetime_str()))
+    elif format == 'excel':
+        data_df.to_excel('data-{}.xlsx'.format(get_datetime_str()))
+    else:
+        data_df.to_json('data-{}.json'.format(get_datetime_str()))
+    return True
+
+
+def get_datetime_str():
+    now = datetime.datetime.now(tz=datetime.timezone(datetime.timedelta(hours=8)))
+    return now.strftime('%Y%m%d-%H%M%S')
+
+#傳送訊息
+def send_line_notify(msg = "傳送訊息"):
+    line_notify_url = "https://notify-api.line.me/api/notify"
+    line_notify_token =os.getenv("TOKEN")
+
+    #Line Auth Header
+    line_notify_header = {
+        'Authorization': 'Bearer {}'.format(line_notify_token)
+    }
+
+
+    # Line Message
+    line_notify_body = {
+        'message': msg
+    }
+
+
+    res = requests.post(line_notify_url, headers= line_notify_header, data = line_notify_body)
+    res_msg = json.loads(res.text)
+
+
+    return res_msg['status']
+
+'''
+Test
+'''
+# x = download_html(target)
+# y = get_article_url(x)
+# z = parser_article_content(y)
+# save_result(z,'excel')
+# print(z)
+send_line_notify("TEST")
+
+
+
+
+
